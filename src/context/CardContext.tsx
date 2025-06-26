4bdcac52843149b8f4987754cb46e8de24bf0bc9
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-06-26 13:38:09 +0200

    (WIP) removed functions for saving cards in storage

--- src/context/CardContext.tsx
+++ src/context/CardContext.tsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import type PokemonType from "~shared/hooks/pokemon.types";
 import { useGetPokemons } from "~shared/hooks/useGetPokemons.hooks";
-import { CardStorage } from "./cardStorage";
 
 type CardContextType = {
 	allCards: PokemonType[] | null;
@@ -13,7 +12,7 @@ const CardContext = createContext<CardContextType>({
 
 export const CardsProvider = ({ children }: { children: React.ReactNode }) => {
 	const { data } = useGetPokemons();
-	const [filteredCards, setFilteredCards] = useState<PokemonType[] | null>(CardStorage.get());
+	const [filteredCards, setFilteredCards] = useState<PokemonType[] | null>(null);
 
 	useEffect(() => {
 		if (filteredCards) return;
@@ -21,10 +20,8 @@ export const CardsProvider = ({ children }: { children: React.ReactNode }) => {
 		if (!data) return;
 
 		const filtered = data.filter((pokemon) => !pokemon.subtypes.includes("TAG TEAM"));
-		// const filtered = data;
 
 		setFilteredCards(filtered);
-		// CardStorage.save(filtered);
 	}, [data, filteredCards]);
 
 	const value = useMemo(
