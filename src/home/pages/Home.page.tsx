726ce061d377c141cc0d9aa4b5d30343190f4fc4
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-06-26 11:39:39 +0200

    made the cards display on the home page

--- src/home/pages/Home.page.tsx
+++ src/home/pages/Home.page.tsx
@@ -1,16 +1,21 @@
 import { PokemonCard } from "~shared/components/pokemonCard/PokemonCard";
 import { useGetPokemons } from "~shared/hooks/useGetPokemons.hooks";
+import style from "./home.module.scss";
 
 export const Home = () => {
 	const { data } = useGetPokemons();
 	console.log(data);
 
 	return (
-		<div>
+		<div className={style["p-home"]}>
 			<h1>HOME</h1>
-			{data?.map((pokemon) => (
-				<PokemonCard key={pokemon.id} pokemon={pokemon} />
-			))}
+			<div className={style["p-home__list"]}>
+				{data?.map((pokemon) => {
+					if (pokemon.set.series == "Base") {
+						return <PokemonCard key={pokemon.id} pokemon={pokemon} />;
+					}
+				})}
+			</div>
 		</div>
 	);
 };
--- src/shared/components/pokemonCard/PokemonCard.tsx
+++ src/shared/components/pokemonCard/PokemonCard.tsx
@@ -1,10 +1,10 @@
-import type { PokemonType } from "~shared/hooks/pokemon.types";
+import type PokemonType from "~shared/hooks/pokemon.types";
 import styles from "./PokemonCard.module.scss";
 
 export const PokemonCard = ({ pokemon }: { pokemon: PokemonType }) => {
 	return (
-		<div className={styles["card"]}>
-			<img src={pokemon.images.large} alt={pokemon.name} />
+		<div>
+			<img className={styles["card"]} src={pokemon.images.large} alt={pokemon.name} />
 		</div>
 	);
 };
