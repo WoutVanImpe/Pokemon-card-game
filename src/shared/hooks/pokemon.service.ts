bc3cd0a650def38626431bbe8e7d7608ae18e3be
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-06-26 11:38:54 +0200

    scoped the api call to only first generation

--- src/shared/hooks/pokemon.service.ts
+++ src/shared/hooks/pokemon.service.ts
@@ -3,18 +3,34 @@ import type PokemonType from "./pokemon.types";
 
 class PokemonService {
 	async getAllPokemons(): Promise<PokemonType[]> {
-		const response = await fetch("https://api.pokemontcg.io/v2/cards", {
-			headers: {
-				"X-Api-Key": ApiKey,
-			},
-		});
+		const baseUrl = `https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]`;
+		const pageSize = 250;
 
-		if (!response.ok) {
-			throw new Error("Error fetching pokemon");
-		}
+		let currentPage = 1;
+		let allCards: PokemonType[] = [];
+		let totalCount = 4200;
 
-		const data = await response.json();
-		return data.data as PokemonType[];
+		do {
+			const response = await fetch(`${baseUrl}&page=${currentPage}&pageSize=${pageSize}`, {
+				headers: {
+					"X-Api-Key": ApiKey,
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error("Error fetching Pokémon data");
+			}
+
+			const data = await response.json();
+			console.log("page" + currentPage + " done");
+
+			allCards = [...allCards, ...data.data];
+			currentPage++;
+		} while (allCards.length < totalCount);
+
+		const pokemons = allCards.filter((card: PokemonType) => card.supertype === "Pokémon");
+		return pokemons;
 	}
 }
+
 export const pokemonService = new PokemonService();
