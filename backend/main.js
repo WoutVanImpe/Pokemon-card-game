6a83c336469aa296dfcd2c61c937fec4e0430e4c
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-06-29 13:44:09 +0200

    added all dependencies to backend file

--- backend/main.js
+++ backend/main.js
@@ -1,2 +1,19 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { MongoClient, ObjectId } = require("mongodb");
+const url = process.env.MDBlink;
+const app = express();
+const myPort = process.env.MDBport;
+
+app.use(cors());
+app.use(express.json());
+
+async function getDatabase() {
+	// makes a new client and passes along the url
+	const client = new MongoClient(url);
+	// connects the client with the database
+	await client.connect();
+	// return de client to the respective collection
+	return client.db("pokemonGame");
+}
